a69e9b533d9c9d434a0ad59a5fbe09ef13f173df
Tob1chhh <muzaloff_ivan@mail.ru>
2025-10-16 11:49:29 +0300

    New Start for project

--- LiveLib/#source/js/script.js
+++
@@ -1,7 +0,0 @@
-const login_button = document.getElementById("login__btn");
-
-const backToTop = document.getElementById("back_to_top");
-backToTop.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.scrollTo({ top: 0, behavior: "smooth" });
-});
